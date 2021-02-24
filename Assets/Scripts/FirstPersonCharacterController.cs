2ee267735cc0e084a113d6c4beb53d947823e666
Zsofia Janvari <janvari.zsofi@gmail.com>
2021-02-24 10:28:45 +0100

    Basic first person movement and camera controls implemented

---
+++ Assets/Scripts/FirstPersonCharacterController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FirstPersonCharacterController : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [SerializeField] private float speed = 10f;
+    private float gravity = -45f;
+    public float jumpHeight = 3f;
+    public Vector3 velocity;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * z + transform.right * x;
+        controller.Move(move.normalized * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        isGrounded = controller.isGrounded;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f;
+        }
+    }
+}
