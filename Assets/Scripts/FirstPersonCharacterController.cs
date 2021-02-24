7c0b4ba3889efd62b27eede613c471828d0a7e37
Zsofia Janvari <janvari.zsofi@gmail.com>
2021-02-24 15:33:21 +0100

    Cleaned up code a little bit, stopped player from moving while mid-air

--- Assets/Scripts/FirstPersonCharacterController.cs
+++ Assets/Scripts/FirstPersonCharacterController.cs
@@ -5,10 +5,12 @@ public class FirstPersonCharacterController : MonoBehaviour
     private CharacterController controller;
 
     [SerializeField] private float speed = 10f;
-    private float gravity = -45f;
-    public float jumpHeight = 3f;
+    [SerializeField] private float gravity = -45f;
+    [SerializeField] public float jumpHeight = 3f;
     public Vector3 velocity;
     public bool isGrounded = false;
+    private bool isXLocked = false;
+    private bool isZLocked = false;
 
     void Start()
     {
@@ -17,22 +19,44 @@ public class FirstPersonCharacterController : MonoBehaviour
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.forward * z + transform.right * x;
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move((move.normalized * speed + velocity) * Time.deltaTime);
-
         isGrounded = controller.isGrounded;
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        // get move direction
+        float x = isXLocked ? 0 : Input.GetAxis("Horizontal");
+        float z = isZLocked ? 0 : Input.GetAxis("Vertical");
+        // inputDirection, h v
+        Vector3 move = transform.forward * z + transform.right * x;
+        Vector3 moveDirection = move.normalized;
+
+        // apply gravity (before jump boost)
+        velocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            // unlock horizontal movement
+            if (isZLocked) isZLocked = false;
+            if (isXLocked) isXLocked = false;
+            
+            // jump boost (will gradually die down)
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            // reset building gravity (after applying gravity)
+            if (velocity.y < 0f)
+            {
+                velocity.y = -2f;
+            }
+        }
+        else
+        {
+            // lock horizontal movement if it halted mid air
+            if (x == 0) isXLocked = true;
+            if (z == 0) isZLocked = true;
         }
 
-        if (isGrounded && velocity.y < 0f)
-        {
-            velocity.y = -2f;
-        }
+        // move the player (final step)
+        controller.Move((moveDirection * speed + velocity) * Time.deltaTime);
     }
 }
