f386398cd19dab499ee2d20408df6945c2c1b283
Zsofia Janvari <janvari.zsofi@gmail.com>
2021-03-11 19:28:46 +0100

    Created collectible prefab

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    /// <summary>
+    /// Can be collected by player or other objects.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // save data
+        Destroy(gameObject);
+        Debug.Log("Collectible destroyed");
+    }
+}
